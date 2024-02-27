1b98ad2656174cdf1a68e2417b7f1adf93cbc08f
shreyk17 <shreyk.11@gmail.com>
2024-02-27 12:34:59 +0530

    resolving the start command issue

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var Express = require("express");
 var MongoClient = require("mongodb").MongoClient;
 var cors = require("cors");
-const multer = require("multer");
 const feeRoute = require("../backend/routes/fee");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
